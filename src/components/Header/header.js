526da996791c391452898412803feb0f3dcc7838
mwlai288 <mwlai288@gmail.com>
2020-08-10 11:19:39 -0400

    work on hamburger links and animation for mobile

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,50 +1,57 @@
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <nav className="wrapper">
-      <a href="#!" className="wrapper__home">
-        Poké Bao
-      </a>
-      <div className="wrapper__burger">
-        <span></span>
-      </div>
-      <div className="wrapper__links">
-        <ul>
-          <li>
-            <a href="#welcome" className="wrapper__links--welcome">
-              Welcome
-            </a>
-          </li>
-          <li>
-            <a href="#about" className="wrapper__links--about">
-              About Us
-            </a>
-          </li>
-          <li>
-            <a href="#menu" className="wrapper__links--menu">
-              Menu
-            </a>
-          </li>
-          <li>
-            <a href="#gallery" className="wrapper__links--gallery">
-              Gallery
-            </a>
-          </li>
-          <li>
-            <a href="#findUs" className="wrapper__links--findus">
-              Find Us
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </header>
-)
+const Header = ({ siteTitle }) => {
+  const [isToggled, setIsToggled] = useState(false)
+  const openClass = isToggled ? "open" : ""
+  return (
+    <header>
+      <nav className="wrapper">
+        <a href="#!" className="wrapper__home">
+          Poké Bao
+        </a>
+        <div onClick={() => setIsToggled(!isToggled)}>
+          <div className={`wrapper__burger ${openClass}`}>
+            <span></span>
+          </div>
+        </div>
+        <div className="wrapper__links">
+          <ul>
+            <li>
+              <a href="#welcome" className="wrapper__links--welcome">
+                Welcome
+              </a>
+            </li>
+            <li>
+              <a href="#about" className="wrapper__links--about">
+                About Us
+              </a>
+            </li>
+            <li>
+              <a href="#menu" className="wrapper__links--menu">
+                Menu
+              </a>
+            </li>
+            <li>
+              <a href="#gallery" className="wrapper__links--gallery">
+                Gallery
+              </a>
+            </li>
+            <li>
+              <a href="#findUs" className="wrapper__links--findus">
+                Find Us
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
+  isToggled: PropTypes.bool.isRequired,
 }
 
 Header.defaultProps = {
