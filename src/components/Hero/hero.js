293e3d9c202752e7d8aa22d6e08e2e33b471f9f0
mwlai288 <mwlai288@gmail.com>
2020-08-10 14:53:07 -0400

    headline box created

--- src/components/Hero/hero.js
+++ src/components/Hero/hero.js
@@ -2,12 +2,15 @@ import React from "react"
 
 const Hero = () => {
   return (
-    <section className="hero">
-      {/* Headline Box */}
-      <h3 className="poop">Hero Component</h3>
-
-      <div className="hero__title">
-        <div className="hero__options">Bowls | Buns | Boba</div>
+    <section>
+      <div className="headline">
+        <div className="headline__container">
+          <h1 className="headline__container--title">Poké Bao</h1>
+          <h4 className="headline__container--options">Poké | Buns | Boba</h4>
+          <button>
+            <a href="#!">Order Now</a>
+          </button>
+        </div>
       </div>
     </section>
   )
